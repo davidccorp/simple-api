f21c8f33c3cba9f59826dbc131e6e85452e41fa9
David Chong <davidchong99@gmail.com>
2025-03-04 06:25:33 +0100

    First draft

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+# Create app
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    # Start server
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8080,
+        log_level="info",
+    )
